3ad0124c2f567f9bef9180f83888a3fb31555068
“Seregey <jovellir@gmail.com>
2018-09-25 09:47:41 +0600

    Add anchors animate

--- app/modules/common.js
+++ app/modules/common.js
@@ -2,6 +2,13 @@ import $ from 'jquery';
 
 const common = () => {
 
+  //anchors
+  $('.anchor').on('click', function(e){
+    e.preventDefault();
+    var elem = $(this).attr('href'),
+        positionscroll = $(elem).offset().top;
+    $('body,html').animate({scrollTop:positionscroll}, 1000);
+  });
 };
 
 export default common();
